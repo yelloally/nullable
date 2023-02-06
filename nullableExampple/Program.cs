e6d63d276fe951c57eec85366fd914a538c553b3
Alona <122108963+yelloally@users.noreply.github.com>
2023-02-06 11:50:45 +0100

    Add files via upload

---
+++ nullableExampple/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace nullableExampple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare a nullable integer variable named x
+            int? x = null;
+
+            // Check if x has a value assigned to it
+            if (x.HasValue)
+            {
+                // If x has a value, print it
+                Console.WriteLine("x has a value of: " + x.Value);
+            }
+            else
+            {
+                // If x is null, print a message indicating it's null
+                Console.WriteLine("x is null");
+            }
+        }
+    }
+}
